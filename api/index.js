5f52588897520dfba4508705ab5393221edd44b4
Reagan Kaijage <rkaijage@gmail.com>
2026-04-03 20:52:06 +0300

    Clean Vercel configuration for TanStack Start

--- api/index.js
+++
@@ -1,9 +0,0 @@
-import { createServer } from 'node:http'
-import { createRequestHandler } from '@tanstack/react-start/server'
-
-const server = createServer(createRequestHandler())
-
-const port = process.env.PORT || 3000
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
